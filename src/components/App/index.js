a83d0ea0ee75aeb3e6ee5b516867b949c36b395a
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-03-28 15:03:07 +0100

    Refactores App component and CSS file.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,9 @@
-import "./App.css";
+import css from "./App.module.css";
 import Scene1 from "../Scene1";
 
 function App() {
   return (
-    <div className="App">
+    <div className={css.App}>
       <h1>test-r3fiber</h1>
       <Scene1></Scene1>
     </div>
--- src/components/Scene1/index.js
+++ src/components/Scene1/index.js
@@ -3,7 +3,7 @@ import { Canvas } from "@react-three/fiber";
 
 function Scene1() {
   function Box() {
-    //this function is called as a Component to render it, is just a React Component?
+    //this function is called as a Component to render it, it's just a React Component?
     return (
       <mesh>
         <boxBufferGeometry attach="geometry" />
@@ -13,12 +13,11 @@ function Scene1() {
   }
 
   return (
-    <>
-      <div>Scene1</div>
+    <div id="Scene1">
       <Canvas>
         <Box />
       </Canvas>
-    </>
+    </div>
   );
 }
 
