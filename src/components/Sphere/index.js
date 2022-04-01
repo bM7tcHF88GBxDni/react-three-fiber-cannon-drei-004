0f14bd188e511606ce4279b3f5a230bdd3d65e85
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-04-01 23:40:16 +0100

    Store destination object position with additional positioning. Added MeshReference.json to view strucutre of Fiber's Mesh wrapper.

--- src/components/Sphere/index.js
+++ src/components/Sphere/index.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { useThree } from "react-three-fiber";
+import * as THREE from "three";
 
 function Sphere() {
   const state = useThree();
@@ -8,13 +9,24 @@ function Sphere() {
   function moveCamera(event) {
     const initialPosition = state.camera.position;
     const initialQuaternion = state.camera.quaternion;
+    //console.log("sphere", sphere);
 
-    const spherePosition = sphere.current.getWorldPosition(); //this is incorrect. look into it.
-    state.camera.position = spherePosition;
+    //const spherePosition = sphere.current.matrix.getPosition();
+    const sphereWorldPosition = sphere.current.matrixWorld.getPosition();
+    //console.log("spherePosition", spherePosition);
+    console.log("sphereWorldPosition", sphereWorldPosition);
+
+    const viewSphereFromPosition = sphereWorldPosition.add(
+      new THREE.Vector3(2, 3, 3) //view sphere from its top right corner
+    );
+    console.log("viewSphereFromPosition", viewSphereFromPosition);
+
+    state.camera.position = viewSphereFromPosition;
     const destinationPosition = state.camera.position;
 
     state.camera.lookAt(sphere);
     const destinationQuaternion = state.camera.quaternion;
+    console.log("destinationQuaternion", destinationQuaternion);
   }
 
   return (
