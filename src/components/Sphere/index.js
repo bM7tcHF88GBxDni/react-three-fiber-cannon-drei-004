9b517b2e8ac1afca5926e8303e77a8ccbdf7b33c
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-04-04 19:21:54 +0100

    Store target sphere's matrix and store calculated position and rotation from matrix, instead of deprecated methods

--- src/components/Sphere/index.js
+++ src/components/Sphere/index.js
@@ -9,24 +9,37 @@ function Sphere() {
   function moveCamera(event) {
     const initialPosition = state.camera.position;
     const initialQuaternion = state.camera.quaternion;
-    //console.log("sphere", sphere);
+    console.log("sphere", sphere); //store the camera's matrix in the future
 
     //const spherePosition = sphere.current.matrix.getPosition();
-    const sphereWorldPosition = sphere.current.matrixWorld.getPosition();
-    //console.log("spherePosition", spherePosition);
-    console.log("sphereWorldPosition", sphereWorldPosition);
 
-    const viewSphereFromPosition = sphereWorldPosition.add(
+    //storing the target sphere's global matrix
+    const sphereMatrix = sphere.current.matrixWorld;
+    console.log("sphere.current.matrixWorld", sphereMatrix);
+
+    //storing sphere's position (Vector3)
+    const positionFromSphereMatrix = new THREE.Vector3().setFromMatrixPosition(
+      sphereMatrix
+    );
+    //storing sphere's rotation (Quaternion)
+    const quaternionFromSphereMatrix =
+      new THREE.Quaternion().setFromRotationMatrix(sphereMatrix);
+
+    //console.log("spherePosition", spherePosition);
+    console.log("positionFromSphereMatrix", positionFromSphereMatrix);
+    console.log("quaternionFromSphereMatrix", quaternionFromSphereMatrix);
+
+    const sphereExternalCamPos = positionFromSphereMatrix.add(
       new THREE.Vector3(2, 3, 3) //view sphere from its top right corner
     );
-    console.log("viewSphereFromPosition", viewSphereFromPosition);
+    console.log("sphereExternalCamPos", sphereExternalCamPos);
 
-    state.camera.position = viewSphereFromPosition;
+    /*     state.camera.position = viewSphereFromPosition;
     const destinationPosition = state.camera.position;
 
     state.camera.lookAt(sphere);
     const destinationQuaternion = state.camera.quaternion;
-    console.log("destinationQuaternion", destinationQuaternion);
+    console.log("destinationQuaternion", destinationQuaternion); */
   }
 
   return (
