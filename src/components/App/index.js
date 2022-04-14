1b537b281c140cabe15360ef083b951adca0ac37
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-04-14 20:44:57 +0100

    Moved Canvas one level higher into root component. Updated index css to render on whole webpage. Added target state in App component.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,49 +1,62 @@
+import { useState } from "react";
+import * as THREE from "three";
+import { useThree, useFrame } from "@react-three/fiber";
+import { Html, Scroll, ScrollControls, OrbitControls } from "@react-three/drei";
+
 import css from "./App.module.css";
-import Scene1 from "../Scene1";
-import { Canvas } from "@react-three/fiber";
-import { Html, Scroll, ScrollControls } from "@react-three/drei";
-import { OrbitControls } from "@react-three/drei";
-import TestReactComponent from "../TestReactComponent";
 import Lighting from "../Lighting";
+import Scene1 from "../Scene1";
 import Sphere from "../Sphere";
+import TestReactComponent from "../TestReactComponent";
 
 function App() {
+  const [target, setTarget] = useState({
+    position: new THREE.Vector3(0, 0, 10),
+    quaternion: new THREE.Quaternion(),
+  });
+
+  // const state = useThree();
+
+  // useFrame((state, dt) => {
+  //   //camera moves to new position
+  //   state.camera.position.lerp(
+  //     target.position,
+  //     THREE.MathUtils.damp(0, 1, 6, dt)
+  //   );
+  //   //camera rotates to new position
+  //   state.camera.quaternion.slerp(
+  //     target.quaternion,
+  //     THREE.MathUtils.damp(0, 1, 6, dt)
+  //   );
+  // });
+
   return (
     <>
-      <div className={css.appContainer}>
-        <Canvas
-          className={css.scene1}
-          pixelRatio={[1, 2]}
-          camera={{ position: [0, 0, 10] }}
-        >
-          {/* frameloop="demand" */}
-          <Lighting />
-          {/* <OrbitControls enableZoom={false} /> */}
-          <ScrollControls
-            className={css.scrollControl}
-            pages={3} // Each page takes 100% of the height of the canvas
-            distance={1} // A factor that increases scroll bar travel (default: 1)
-            damping={8} // Friction, higher is faster (default: 4)
-            horizontal={true} // Can also scroll horizontally (default: false)
-            infinite={false} // Can also scroll infinitely (default: false)
-          >
-            <Scroll>
-              <Html>
-                <h1>react-three-fiber-cannon-drei-004</h1>
-              </Html>
-              <Scene1></Scene1>
-            </Scroll>
-            <Scroll></Scroll>
-            <Scroll>
-              <Html>
-                this is between the third Scroll tags
-                <TestReactComponent></TestReactComponent>
-              </Html>
-              <Sphere />
-            </Scroll>
-          </ScrollControls>
-        </Canvas>
-      </div>
+      <Lighting />
+      {/* <OrbitControls enableZoom={false} /> */}
+      <ScrollControls
+        className={css.scrollControl}
+        pages={3} // Each page takes 100% of the height of the canvas
+        distance={1} // A factor that increases scroll bar travel (default: 1)
+        damping={8} // Friction, higher is faster (default: 4)
+        horizontal={true} // Can also scroll horizontally (default: false)
+        infinite={false} // Can also scroll infinitely (default: false)
+      >
+        <Scroll>
+          <Html>
+            <h1>react-three-fiber-cannon-drei-004</h1>
+          </Html>
+          <Scene1></Scene1>
+        </Scroll>
+        <Scroll></Scroll>
+        <Scroll>
+          <Html>
+            this is between the third Scroll tags
+            <TestReactComponent></TestReactComponent>
+          </Html>
+          <Sphere />
+        </Scroll>
+      </ScrollControls>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { Canvas } from "@react-three/fiber";
+
+import css from "./index.module.css";
 import App from "./components/App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className={css.appContainer}>
+    <Canvas pixelRatio={[1, 2]} camera={{ position: [0, 0, 10] }}>
+      {/* frameloop="demand" */}
+      <App />
+    </Canvas>
+  </div>
 );
