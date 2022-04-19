25dda4b5499812fb54447e18d08f0c2d09331820
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-04-19 23:49:52 +0100

    Add Gradient Texture to mesh material. Geometry mesh properties not working properly.

--- src/components/FloatingComponent/index.js
+++ src/components/FloatingComponent/index.js
@@ -9,9 +9,22 @@ function FloatingComponent() {
       rotationIntensity={1} // XYZ rotation intensity, defaults to 1
       floatIntensity={1} // Up/down float intensity, defaults to 1
     >
-      <mesh position={[8, 0, -4]} rotation={[0, 0, 0]}>
-        <torusKnotBufferGeometry attach="geometry" />
-        <meshPhongMaterial attach="material"></meshPhongMaterial>
+      <mesh position={[8, 0, -4]}>
+        <torusKnotGeometry
+          radius={10}
+          tube={2.5}
+          tubularSegments={30}
+          radialSegments={8}
+          p={1}
+          q={6}
+        />
+        <meshPhongMaterial attach="material">
+          <GradientTexture
+            stops={[0, 0.25, 0.5, 0.75, 1]} // As many stops as you want
+            colors={["white", "blue", "grey", "cyan", "darkred"]} // Colors need to match the number of stops
+            size={1024} // Size is optional, default = 1024
+          />
+        </meshPhongMaterial>
       </mesh>
     </Float>
   );
